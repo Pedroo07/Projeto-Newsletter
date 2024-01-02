0dd1c1cfeebf30a1fbaf200fb75a40e0ef37ee7f
Pedroo <pedrohenriquemoalive@gmail.com>
2024-01-02 14:37:19 -0300

    Iniciando Projeto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Form from "./components/Form"
+
+const App = () => {
+  return (
+    <>
+    <h1 className="text-red-600">Bla</h1>
+    <p>Inscreva-se na Newsletter e se matenha informado!</p>
+    <div>
+      <Form />
+    </div>
+    <p>Ao se inscrever você passará a receber nossos e-mails com as mellhores informações.</p>
+    </>
+  )
+}
+
+export default App
