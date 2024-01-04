34b0729fedbe77b9422aa3eb54ffa0acb8889cc6
Pedroo <pedrohenriquemoalive@gmail.com>
2024-01-04 11:11:54 -0300

    Criando logica de validação

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,49 @@
+import { useState, FormEvent } from "react"
+import { User } from "../types/User"
+import { validate } from "../utils/validate"
 
-function Form() {
+const Form = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [agree, setAgree] = useState(false)
+
+  const [error, setError] = useState<User | null>(null)
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    const data: User = {
+      name,
+      email,
+      agree
+    }
+
+    const validateErrors = validate(data)
+
+    if (Object.keys(validateErrors).length > 0) {
+      alert('tem erros')
+    }
+
+  }
   return (
-   <form className="flex flex-col gap-3">
-    <div className="flex flex-col">
-      <label className="text-sn"  htmlFor="name">Nome</label>
-        <input type="text" placeholder="Digite seu nome" className="rounded-lg  py-2 px-2 text-sm  placeholder:text-stone-400 placeholder:text-sm" />
-    </div>
-    <div className="flex flex-col">
-      <label className="text-sn"  htmlFor="email">E-mail</label>
-        <input type="email" placeholder="Insira seu melhor e-mail" className="rounded-lg  py-2 px-2 text-sm  placeholder:text-stone-400 placeholder:text-sm" />
-    </div>
-    <div className="flex flex-col">
-      <a href="" className="text-xs underline mb-2">Leia os termos</a>
-      <div className="flex gap-2 items-center">
-        <input type="checkbox" className="cursor-pointer" />
-        <label className="text-sn"  htmlFor="agree">Concordo com os termos</label>
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+      <div className="flex flex-col">
+        <label className="text-sn" htmlFor="name">Nome</label>
+        <input type="text" placeholder="Digite seu nome" className="rounded-lg  py-2 px-2 text-sm  placeholder:text-stone-400 placeholder:text-sm"  value={name} onChange={(e) => setName(e.target.value)}/>
       </div>
-    </div>
-    <button type="submit" className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white">Cadastrar</button>
-   </form>
+      <div className="flex flex-col">
+        <label className="text-sn" htmlFor="email">E-mail</label>
+        <input type="email" placeholder="Insira seu melhor e-mail" className="rounded-lg  py-2 px-2 text-sm  placeholder:text-stone-400 placeholder:text-sm" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="flex flex-col">
+        <a href="" className="text-xs underline mb-2">Leia os termos</a>
+        <div className="flex gap-2 items-center">
+          <input type="checkbox" className="cursor-pointer" checked={agree} onChange={(e) => setAgree(e.target.checked)}/>
+          <label className="text-sn" htmlFor="agree">Concordo com os termos</label>
+        </div>
+      </div>
+      <button type="submit" className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white">Cadastrar</button>
+    </form>
   )
 }
 
